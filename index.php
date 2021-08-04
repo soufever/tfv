e39a1b82bcea6c30803bce90cc4074dc13b0c86f
FeveR <81459926+soufever@users.noreply.github.com>
2021-08-04 14:26:21 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Torneio FeveR de VALORANT</title>
+  <meta name="description" content="Página do Torneio FeveR de VALORANT">
+  <meta name="author" content="FeveR">
+
+  <meta property="og:title" content="Torneio FeveR de VALORANT">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="http://tfv.soufever.live">
+  <meta property="og:description" content="Página do Torneio FeveR de VALORANT">
+  <meta property="og:image" content="image/image.png">
+
+  <link rel="icon" href="image/favicon.ico">
+  <link rel="icon" href="/imagefavicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="image/apple-touch-icon.png">
+
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+  <!-- your content here... -->
+  
+</body>
+</html>
